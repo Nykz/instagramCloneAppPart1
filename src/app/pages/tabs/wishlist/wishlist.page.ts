cf9eb1b5588b140c3395386a398892d6765683ea
DESKTOP-N988CNC\Nyk <nykz786@gmail.com>
2021-06-16 11:26:22 +0530

    first commit

---
+++ src/app/pages/tabs/wishlist/wishlist.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wishlist',
+  templateUrl: './wishlist.page.html',
+  styleUrls: ['./wishlist.page.scss'],
+})
+export class WishlistPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
